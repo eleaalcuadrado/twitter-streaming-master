707692a0a6ff9a25912cf79b46ad4429af5f9645
cristianfervas1992 <cristian.fernandez@usach.cl>
2016-11-13 02:20:28 -0300

    Update ServiceFindTweets.java

--- src/main/java/service/ServiceFindTweets.java
+++ src/main/java/service/ServiceFindTweets.java
@@ -226,4 +226,78 @@ public class ServiceFindTweets {
 		}
         return Integer.toString(list.size());
     }
+
+	
+	//FUNCION QUE DETERMINA LA CANTIDAD DE TWEETS EN UNA FECHA ESPECIFICA QUE ADEMAS LE ENTRA UN i QUE IRA RESTANDO LOS DIAS, IGUAL
+	//NO SE IMPLEMENTA ESO DE SI VA A CAMBIAR EL MES SI SE CONSULTA POR EJEMPLO UN DIA 1 O 2 Y LA SEMANA ESTA ENTRE 2 MESES
+    public int cantidad(String compania, String fecha, int i) {
+    	int day;
+        MongoClient mongo = new MongoClient("localhost", 27017);
+        DB db = mongo.getDB("twitterdb");
+        DBCollection collection = db.getCollection("todo");
+    	List<DBObject> list = new ArrayList<DBObject>();
+        BasicDBObject whereQuery = new BasicDBObject();
+
+        String[] corte = fecha.split("-");
+		String mes = corte[0];
+		String dia = corte[1];
+		String anho = corte[2];
+		//AQUI SE VA RESTANDO DE DIA EN DIA
+		day = (Integer.parseInt(dia))-i;
+		
+		whereQuery.put("created_at", 
+			new BasicDBObject("$regex", String.format(".*((?i)%s %s).*((?i)%s).*",mes,String.valueOf(day),anho)) );
+		whereQuery.put("text", 
+			new BasicDBObject("$regex", String.format(".*((?i)%s).*",compania)) );
+		BasicDBObject fields = new BasicDBObject();
+
+		DBCursor cursor = collection.find(whereQuery);
+		while (cursor.hasNext()) {
+			list.add(cursor.next());
+		}
+        return list.size();
+    }
+    
+    
+    
+	@GET
+    @Path("fecha/{fecha}/grafico")
+    @Produces({"application/json"})
+	public String crearGrafico(@PathParam("fecha") String fecha){
+		MongoClient mongo = new MongoClient("localhost", 27017);
+        DB db = mongo.getDB("twitterdb");
+        DBCollection collection = db.getCollection("todo");
+    	List<DBObject> list = new ArrayList<DBObject>();
+        BasicDBObject whereQuery = new BasicDBObject();
+        int i = 0;
+        int repetido;
+        String companias[] = {"movistar", "vtr", "wom", "virgin", "entel","claro"};
+        String company;
+        //String[] corte = fecha.split("-");
+	//String mes = corte[0];
+	//String dia = corte[1];
+	//String anho = corte[2];
+	//int day = Integer.parseInt(dia);
+	//CICLO FOR QUE PREGUNTE POR COMPAÑIA LOS TWEETS 
+	for(i=0;i<6;i++){
+		company = companias[i];
+		//whereQuery.put("created_at", 
+		//		new BasicDBObject("$regex", String.format(".*((?i)%s %s).*((?i)%s).*",mes,String.valueOf(day),anho)) );
+		whereQuery.put("text", 
+			new BasicDBObject("$regex", String.format(".*((?i)%s).*",company)) );
+		//AQUI DEBIESE ENTREGAR LA CANTIDAD DE TWEETS DE LA COMPAÑIA INDICADA PERO NO SE COMO AGREGARLO A UN NUEVO JSON
+		//CON LA ESTRUCTURA Q PIDE EL SERGIO :/
+		repetido=cantidad(company,fecha,i);
+		DBCursor cursor = collection.find(whereQuery);
+		while (cursor.hasNext()) {
+			list.add(cursor.next());
+			}
+			
+		}
+	//BasicDBObject fields = new BasicDBObject();
+	return list.toString();
+        }
+
+
+	
 }
